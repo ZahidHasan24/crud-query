6c557ebb33ae4c7347ad06633827c67dcfd735c3
Zahid Hasan <hzahid2999@gmail.com>
2022-10-06 00:44:54 +0600

    add creatbook api with form

--- client/src/service/index.js
+++ client/src/service/index.js
@@ -16,6 +16,22 @@ export const removeBook = async (id) => {
   return true;
 };
 
+export const createBook = async ({ ...data }) => {
+  const res = await fetch(`${process.env.REACT_APP_API_URL}/books`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) {
+    throw new Error(res.json().message);
+  }
+
+  return res.json();
+};
+
 export const getBook = async ({ queryKey }) => {
   const [_key, { id }] = queryKey;
   const res = await fetch(`${process.env.REACT_APP_API_URL}/books/${id}`);
